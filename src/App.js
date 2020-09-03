4094ccc8afb0a81f9d65f82239f2e46199c630e4
Guga Gongadze <gugongadze@gmail.com>
2020-09-03 13:41:11 +0400

    feat: first chunk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+const API_URL = 'http://localhost:5000/v1/video/upload/'
 
 function App() {
+  const [loaded, setLoaded] = useState(0)
+
+  const onFileDrop = (event) => {
+    const file = event.target.files[0]
+    const chunkSize = 4000000 // 4MB chunk
+    const total = file.size
+    console.log("TOTAL SIZE", total / 1_000_000, 'MB')
+    const reader = new FileReader();
+    const slice = file.slice(0, chunkSize);
+
+    reader.readAsBinaryString(slice);
+    console.log('Started uploading file "' + file.name + '"');
+
+    reader.onload = async function (e) {
+      try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        body: slice,
+      })
+        
+      console.log(response);
+      } catch (error) {
+      console.log("onFileDrop -> error", error)
+        
+      }
+
+
+
+    }
+
+
+      //   $.ajax({
+      //       url: "http://api/url/etc",
+      //       type: "POST",
+      //       data: slice,
+      //       processData: false,
+      //       contentType: false,
+      //       error: (function (errorData) {
+      //           console.log(errorData);
+      //           alert("Video Upload Failed");
+      //       })
+      //   }).done(function (e){
+      //      loaded += chunkSize;
+      //      var percentLoaded = Math.min((loaded / total) * 100, 100);
+      //      console.log('Uploaded ' + Math.floor(percentLoaded) + '% of file "' + file.name + '"');
+
+      //      //Read the next chunk and call 'onload' event again
+      //      if (loaded <= total) {
+      //        slice = file.slice(loaded, loaded + chunkSize);
+      //        reader.readAsBinaryString(slice);
+      // } else {
+      //    loaded = total;
+      //      console.log('File "' + file.name + '" uploaded successfully!');
+      //      }
+      //   }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label htmlFor="video">Upload some video</label>
+        <input onChange={onFileDrop} type="file" name="video" id="video" />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
